2dffeb6c5d55a9d3894386d75d6b6d84c74eaba6
Joy Gacheri <joydouglas011@gmail.com>
2024-11-10 16:00:55 +0300

    Django Assignment

---
+++ Agri/views.py
@@ -0,0 +1,26 @@
+from urllib import request
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,  'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def blogdetails(request):
+    return render(request, 'blog-details.html')
+
